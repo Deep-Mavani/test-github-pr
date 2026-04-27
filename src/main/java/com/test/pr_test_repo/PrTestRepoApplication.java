05aa667bd394fd5fcd83c08342e185dda21f2196
Deep-Mavani <deepmavani16@gmail.com>
2026-04-27 14:33:41 +0530

    first commit

---
+++ src/main/java/com/test/pr_test_repo/PrTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.test.pr_test_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrTestRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrTestRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/pr_test_repo/PrTestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.pr_test_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrTestRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
